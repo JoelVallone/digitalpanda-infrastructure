fcc25bfd042878bc83e32f8a9946dd686cc99fbc
Joël Vallone <joel.vallone@gmail.com>
2017-12-22 14:51:05 +0100

    Use docker compose to deploy services

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -6,14 +6,19 @@
 SCRIPT_NAME=$0
 SCRIPT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 INITIAL_FOLDER=$(pwd)
-echo "Build & push infrastructure"
-cd ${SCRIPT_FOLDER}/../digitalpanda-backend/scripts/
-./build.sh
-cd ${SCRIPT_FOLDER}/../digitalpanda-frontend/scripts/
-./build-prod.sh
-sudo ${SCRIPT_FOLDER}/docker/scripts/push.sh backend-java frontend-angular2
-cd ${INITIAL_FOLDER}
-
+echo "Build software"
+cd ${SCRIPT_FOLDER}/../digitalpanda-iot/
+mvn clean install
+cd ${SCRIPT_FOLDER}/../digitalpanda-backend/
+./scripts/build.sh
+cd ${SCRIPT_FOLDER}/../digitalpanda-frontend/
+./scripts/build-prod.sh
+echo "Build containers"
+cd ${SCRIPT_FOLDER}/../digitalpanda-infrastructure/docker/compose/
+#sudo ${SCRIPT_FOLDER}/docker/scripts/push.sh backend-java frontend-angular2
+sudo docker-compose -f compose.yml build
 echo "Restart containers"
-${SCRIPT_FOLDER}/docker/scripts/deploy.sh digitalpanda-backend-0 digitalpanda-frontend-0
+sudo docker-compose -f compose.yml up -d
+cd ${INITIAL_FOLDER}
+#${SCRIPT_FOLDER}/docker/scripts/deploy.sh digitalpanda-backend-0 digitalpanda-frontend-0
 
