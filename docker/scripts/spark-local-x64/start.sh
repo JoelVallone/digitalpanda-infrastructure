b97b661b800e430d38acc0cc22cec470aef397eb
Joël Vallone <joel.vallone@gmail.com>
2018-01-27 14:29:35 +0100

    finalized hadoop-hdfs local docker deploy with basic configuration

--- docker/scripts/spark-local-x64/start.sh
+++ docker/scripts/spark-local-x64/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+#set -e
 
 SCRIPT_FOLDER="$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
 DOCKER_IMAGES=$SCRIPT_FOLDER/../../images
@@ -12,6 +12,23 @@ if [ -n "$(sudo docker ps -q)" ]; then
 fi
 }
 
+function cleanupTrap {
+    echo "/!\\ CLEANUP DUE TO FAILURE /!\\"
+    cleanup
+}
+
+function clearDataDirs {
+    rm -rf $SCRIPT_FOLDER/data-mount/hdfs/master/data
+    rm -rf $SCRIPT_FOLDER/data-mount/hdfs/slave/data
+    rm -rf $SCRIPT_FOLDER/data-mount/logs
+}
+
+function createDataDirs {
+    mkdir -p $SCRIPT_FOLDER/data-mount/hdfs/master/data
+    mkdir -p $SCRIPT_FOLDER/data-mount/hdfs/slave/data
+    mkdir -p $SCRIPT_FOLDER/data-mount/logs
+}
+
 function msg {
 txt=\
 "=========================================================\n
@@ -20,36 +37,41 @@ txt=\
 echo -e "" $txt
 }
 
-trap echo -e "failure trap: \n" && cleanup EXIT
+#trap cleanup EXIT
+trap cleanup SIGQUIT
 
 echo "STARTING SPARK LOCALLY"
 cleanup
+clearDataDirs || true
+createDataDirs
 
 msg "Build docker images"
-sudo docker build -t hadoop-hdfs:latest ${DOCKER_IMAGES}/hadoop-hdfs
+sudo docker build -t hadoop-hdfs:latest ${DOCKER_IMAGES}/hadoop
 
 msg "Start docker images"
 
-
-
 HDFS_MASTER_DOCKER_ID=$(sudo docker run \
     --name=hadoop-hdfs-master \
+    -v $SCRIPT_FOLDER/data-mount/:/opt/hadoop/ext \
     --net=host \
     -d -t hadoop-hdfs:latest)
 
 HDFS_SLAVE_DOCKER_ID=$(sudo docker run \
     --name=hadoop-hdfs-slave \
+    -v $SCRIPT_FOLDER/data-mount/:/opt/hadoop/ext \
     --net=host \
     -d -t hadoop-hdfs:latest)
 
 msg "Start Spark cluster"
 sudo docker exec hadoop-hdfs-master bash -c '"$HADOOP_PREFIX"/bin/hdfs namenode -format hadoop-hdfs' 
-sudo docker exec hadoop-hdfs-master bash -c '"$HADOOP_PREFIX"/sbin/hadoop-daemon.sh --config $HADOOP_BASE/config start namenode' 
-sudo docker exec hadoop-hdfs-slave bash -c '"$HADOOP_PREFIX"/sbin/hadoop-daemon.sh  --config $HADOOP_BASE/config start datanode'
-sudo docker ps
+sudo docker exec hadoop-hdfs-master bash -c '"$HADOOP_PREFIX"/sbin/hadoop-daemon.sh start namenode' 
+sudo docker exec hadoop-hdfs-slave bash -c '"$HADOOP_PREFIX"/sbin/hadoop-daemon.sh  start datanode'
 
-echo "STARTED SPARK LOCALLY: "
+echo -e "\n\n"
+msg "STARTED SPARK LOCALLY: "
+sudo docker ps
+echo "=============================================="
 echo " -> hdfs namenode page: http://127.0.0.1:50070"
 
-firefox http://127.0.0.1:50070
+firefox http://127.0.0.1:50070 || true
 
