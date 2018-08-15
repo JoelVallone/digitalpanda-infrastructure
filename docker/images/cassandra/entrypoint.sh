f38a6ac681f0cf90086b391e805232fe39428bb4
Joël Vallone <joel.vallone@gmail.com>
2018-08-15 10:59:56 +0200

    setting up cassandra in docker compose

--- docker/images/cassandra/entrypoint.sh
+++ docker/images/cassandra/entrypoint.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
-function startCassandra {
+function log {
+    echo "$(date) - $1"
+}
+
+function startForegroundCassandra {
     CASSANDRA_CONF=file:///${CASSANDRA_CONF_DIR}
     export CASSANDRA_CONF
 
     JVM_OPTS="-Dcassandra.logdir=${CASSANDRA_LOG_DIR} -Dlogback.configurationFile=${CASSANDRA_CONF_DIR}/logback.xml"
     export JVM_OPTS
-
-    ${CASSANDRA_BIN}/bin/cassandra -R -p ${CASSANDRA_CONF_DIR}/cassandra.pid &> /dev/null
-    chmod a+r ${CASSANDRA_CONF_DIR}/cassandra.pid
+    log "start cassandra"
+    ${CASSANDRA_BIN}/bin/cassandra -f -R -p ${CASSANDRA_CONF_DIR}/cassandra.pid &> /dev/nul
 }
 
 function stopCassandra {
     kill $(cat ${CASSANDRA_CONF_DIR}/cassandra.pid)
     sleep 5
-    echo "cassandra stopped"
+    log "cassandra stop"
 }
 
-if [ "${CONTAINER_AUTO_START:-false}" = "true" ]; then
+#if [ "${CONTAINER_AUTO_START:-false}" = "true" ]; then
     trap stopCassandra SIGTERM
     trap stopCassandra SIGINT
-    startCassandra
-    echo "cassandra started - press enter to stop"
-    read val
+    startForegroundCassandra
     stopCassandra
-else
-    echo "container started - press enter to stop"
-    read val
-fi
+#else
+#    log "cassandra container started"
+#    tail -f /dev/null
+#fi
 
+log "end of entrypoint.sh"
 
